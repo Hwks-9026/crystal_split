81adba50023a33b060eb2bd3b933ae171daf8cb1
Davis Amaral <amaral@pxgpu04.slac.stanford.edu>
2026-08-11 13:05:49 -0700

    improved setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+set -e
+
+# module load python
 
 python -m venv .venv
 
+
+# NECCESARY C LIBRARIES
+./get_libffi.sh
+./get_libssl.sh
+# ADD PATH EXPORT TO VENV, EACH TIME VENV IS ACTIVATED, LIBRARIES WILL BE LOADED
+grep -qxF 'export LD_LIBRARY_PATH="$HOME/.local/lib:${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"' .venv/bin/activate || \
+  echo 'export LD_LIBRARY_PATH="$HOME/.local/lib:${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"' >> .venv/bin/activate
+
 source .venv/bin/activate
+pip install --upgrade pip
 
 pip install --no-user -r requirements.txt
 pip install maturin
 
 cd diffraction_sim && maturin develop --release
 
+
 echo "All Set Up!"
+
+
